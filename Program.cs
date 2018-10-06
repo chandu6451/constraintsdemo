5ecb45bf5f386a8e5ae073ab807a7b8106ed038c
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 14:48:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoonstraintsDemo
+{
+    public class MyGenClass<T> where T : class
+    { }
+
+    public struct Student
+    {
+        public int RollNo { get; set; }
+        public string StudName { get; set; }
+    }
+
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string EmpName { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGenClass<int> objint = new MyGenClass<int>();
+            MyGenClass<Student> objStud = new MyGenClass<Student>();
+            MyGenClass<char> objChar = new MyGenClass<char>();
+            MyGenClass<byte> objByte = new MyGenClass<byte>();
+
+            MyGenClass<object> obj = new MyGenClass<object>();
+            MyGenClass<string> objstr = new MyGenClass<string>();
+            MyGenClass<Employee> objemp = new MyGenClass<Employee>();
+        }
+    }
+}
